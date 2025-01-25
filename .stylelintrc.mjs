85c946ec52f4ba439faa1d7a71fccf8efd5772b5
vralle <vitaliy.ralle@gmail.com>
2025-01-25 22:04:30 +0100

    chore: Project wide stylelint config

---
+++ .stylelintrc.mjs
@@ -0,0 +1,23 @@
+/**
+ * Stylelint options
+ * @see https://stylelint.io/user-guide/configure/
+ * @type {import('stylelint').Config}
+ */
+export default {
+  extends: "stylelint-config-standard",
+  cache: true,
+  ignoreFiles: [
+    "**/dist/**",
+    "**/node_modules/**",
+  ],
+  overrides: [
+    {
+      files: [
+        "*.html",
+      ],
+      extends: [
+        "stylelint-config-html",
+      ],
+    },
+  ],
+};
